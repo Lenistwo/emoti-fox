43e680228231fce3b36b6245774f03985bd20e52
Kacper Fal <kacperek1003@gmail.com>
2020-04-03 18:59:23 +0200

    Initial Commit

---
+++ src/main/java/pl/lenistwo/emotifox/EmotifoxApplication.java
@@ -0,0 +1,13 @@
+package pl.lenistwo.emotifox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotifoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotifoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lenistwo/emotifox/EmotifoxApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lenistwo.emotifox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotifoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
